ece7821ac45fb131cd4ac91891ef2ed46425f0f1
Unclaimedd <unclaimedgamer@gmail.com>
2022-03-17 00:56:49 +1300

    IHateCarpet

---
+++ IHateCarpet/index.js
@@ -0,0 +1,33 @@
+const BlockPoss = Java.type("net.minecraft.util.BlockPos")
+const getDistance = (x1, y1, z1, x2, y2, z2) => Math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
+const setAir = (x, y, z) => World.getWorld().func_175698_g(new BlockPoss(x, y, z))
+const setBlockState = (x, y, z, state) => World.getWorld().func_175656_a(new BlockPoss(x, y, z), state)
+
+let carpets = []
+register("tick", () => {
+    let pX = parseInt(Player.getX())
+    let pY = parseInt(Player.getY())
+    let pZ = parseInt(Player.getZ())
+    for (let x = pX-2; x <= pX+2; x++) {
+        for (let y = pY-2; y <= pY+2; y++) {
+            for (let z = pZ-2; z <= pZ+2; z++) {
+                let block = World.getBlockAt(x, y, z)
+                if (!block || block.type.getID() !== 171) continue
+                carpets.push([block.getState(), x, y, z])
+                setAir(x, y, z)
+            }
+        }
+    }
+})
+
+register("tick", () => {
+    for (let i = 0; i < carpets.length; i++) {
+        let x = carpets[i][1]
+        let y = carpets[i][2]
+        let z = carpets[i][3]
+        if (getDistance(Player.getX(), Player.getY(), Player.getZ(), x, y, z) >= 3) {
+            setBlockState(x, y, z, carpets[i][0])
+            carpets.splice(i, 1)
+        }
+    }
+})
