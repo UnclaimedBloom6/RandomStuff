9cf19bb6d8bbc18f1a4f342b0cd6eadfce3b53cf
snowguy1252 <36537859+snowguy1252@users.noreply.github.com>
2024-06-09 01:17:39 -0500

    Update index.js to work with Etherwarp Conduit
    (also doesnt sneak if conduit)

--- LeftClickEtherwarp/index.js
+++ LeftClickEtherwarp/index.js
@@ -13,9 +13,9 @@ const isHoldingEtherwarpItem = () => {
     const held = Player.getHeldItem()
     const sbId = getSkyblockItemID(held)
 
-    if (sbId !== "ASPECT_OF_THE_END" && sbId !== "ASPECT_OF_THE_VOID") return false
+    if (sbId !== "ASPECT_OF_THE_END" && sbId !== "ASPECT_OF_THE_VOID" && sbId !== "ETHERWARP_CONDUIT") return false
     
-    return held.getNBT()?.toObject()?.tag?.ExtraAttributes?.ethermerge == 1
+    return held.getNBT()?.toObject()?.tag?.ExtraAttributes?.ethermerge == 1 || sbId == "ETHERWARP_CONDUIT"
 }
 
 const pogObj = new PogObject("LeftClickEtherwarp", {
@@ -31,6 +31,8 @@ register("command", () => {
 
 // Left click detection
 register(MouseEvent, (event) => {
+    const held = Player.getHeldItem()
+    const item = getSkyblockItemID(held)
     if (!pogObj.enabled) return
 
     const btn = event.button
@@ -41,7 +43,7 @@ register(MouseEvent, (event) => {
     cancel(event)
 
     // Don't press the sneak key down if you're already sneaking
-    const shouldSneak = !Player.isSneaking()
+    const shouldSneak = !Player.isSneaking() && item !== "ETHERWARP_CONDUIT"
     if (shouldSneak) sneakKey.setState(true)
     
     Client.scheduleTask(0, () => {
@@ -50,4 +52,3 @@ register(MouseEvent, (event) => {
     })
 
 })
-
